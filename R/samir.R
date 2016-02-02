c37ce4e88d745d789ff60e93b4bb14b258ae8c0d
Hlebak  Samantha M <hlebaksm@mathR.math.appstate.edu>
2016-02-01 20:16:51 -0500

    problems

---
+++ R/samir.R
@@ -0,0 +1,8 @@
+#' @description Miscelaneous functions for testing code in class.
+#' @details You shuffle cards or roll dice
+#' @docType package
+#' @name samir-package
+#' @title Functions for shuffle and dice rolling
+#' @section My Custom Section: Add additional information here.
+#' @keywords roll, dice
+NULL
