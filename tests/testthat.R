5163a55c963e6030cf153b13ff68d7105b812e92
Hlebak  Samantha M <hlebaksm@mathR.math.appstate.edu>
2016-02-22 20:19:08 -0500

    Updated with travis file

---
+++ tests/testthat.R
@@ -0,0 +1,24 @@
+library(testthat)
+library(samir)
+
+test_check("samir")
+
+library(simplemathr)
+context("Numbers OK")
+
+test_that("answers are correct",{
+  expect_equal(sq(10), 10^2)
+  expect_equal(sq(20), 20^2)
+  #expect_equal(RP(3, 3), 3^3)
+  #expect_equal(RP(2, 4), 2^4)
+  #expect_equal(RP(NA, 10), NA_integer_)
+  expect_less_than(roll(1:20), 2*20 + 1)
+  expect_more_than(roll(), 1)
+})
+#devtools::use_test_that()
+#devtools::test
+
+
+
+
+
